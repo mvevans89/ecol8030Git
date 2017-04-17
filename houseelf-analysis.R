7819a3321f08b7b5069cdb387364efc071a0f9fc
mvevans89 <mvevans89@gmail.com>
2017-04-16 21:36:49 -0400

    create function to calculate GC content

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,3 +1,11 @@
 #This document analyzes house elves
-
+library(stringr)
 ears <- read.csv("data/houseelf-earlength-dna-data_1.csv", stringsAsFactors = F)
+
+calcGC <- function(sequence){
+  sequence <- str_to_upper(sequence)
+  GCcontent <- (str_count(sequence, "C") + str_count(sequence, "G"))/str_length(sequence)
+  return(GCcontent)
+}
+
+calcGC(ears$dnaseq[1])

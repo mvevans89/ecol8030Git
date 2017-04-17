d14cf6ebdfa918aab067efd891e4f59a06f2b160
mvevans89 <mvevans89@gmail.com>
2017-04-16 21:30:23 -0400

    rename earlength csv file and update R script to read in renamed csv

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,3 +1,3 @@
 #This document analyzes house elves
 
-ears <- read.csv("data/houseelf-earlength-dna-data.csv", stringsAsFactors = F)
+ears <- read.csv("data/houseelf-earlength-dna-data_1.csv", stringsAsFactors = F)
